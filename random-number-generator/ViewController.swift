4f3207b56a36058cc5093938b8c4888910de6313
Edson <edson_neto1996@hotmail.com>
2020-12-15 11:36:22 -0300

    Finished

--- random-number-generator/ViewController.swift
+++ random-number-generator/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var generatedNumberLabel: UILabel!
+    @IBOutlet weak var minRangeField: UITextField!
+    @IBOutlet weak var maxRangeField: UITextField!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+        
+    
 
 
+    @IBAction func generateButton(_ sender: UIButton) {
+        let minRang = Int(minRangeField.text!)!
+        let maxRang = Int(maxRangeField.text!)!
+                
+        let generatedRandom = Int.random(in: minRang...maxRang)
+
+        generatedNumberLabel.text = String(generatedRandom)
+        
+        
+    }
+    
 }
 
